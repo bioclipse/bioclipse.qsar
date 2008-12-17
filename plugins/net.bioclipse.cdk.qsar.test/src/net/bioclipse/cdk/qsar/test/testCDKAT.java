beff0c98ea97ff4b8d2d69ea657e43f196461e43
carl_masak <carl_masak@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2008-12-17 16:33:40 +0000

    turned line-initial tab chars into spaces, and removed whitespace from otherwise empty lines
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@8084 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.cdk.qsar.test/src/net/bioclipse/cdk/qsar/test/testCDKAT.java
+++ plugins/net.bioclipse.cdk.qsar.test/src/net/bioclipse/cdk/qsar/test/testCDKAT.java
@@ -1,7 +1,5 @@
 package net.bioclipse.cdk.qsar.test;
-
 import java.io.InputStream;
-
 import org.openscience.cdk.atomtype.CDKAtomTypeMatcher;
 import org.openscience.cdk.exception.CDKException;
 import org.openscience.cdk.interfaces.IAtom;
@@ -16,52 +14,36 @@ import org.openscience.cdk.tools.CDKHydrogenAdder;
 import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
 import org.openscience.cdk.tools.manipulator.AtomTypeManipulator;
 import org.openscience.cdk.tools.manipulator.ChemFileManipulator;
-
 public class testCDKAT {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-
+        /**
+         * @param args
+         */
+        public static void main(String[] args) {
         InputStream ins = testCDKAT.class.getResourceAsStream("/testFiles/0037.cml");
-        
         CMLReader reader=new CMLReader(ins);
-        
         IChemFile chemFile = new org.openscience.cdk.ChemFile();
-
-
         //Read file
         try {
             chemFile=(IChemFile)reader.read(chemFile);
         } catch (CDKException e) {
         	e.printStackTrace();
         }
-        
         IAtomContainer container = ChemFileManipulator.getAllAtomContainers(chemFile).get(0);
-        
-		try {
-
-			CDKAtomTypeMatcher matcher = CDKAtomTypeMatcher.getInstance(container.getBuilder());
-	        for (IAtom atom : container.atoms()) {
-	            IAtomType matched = matcher.findMatchingAtomType(container, atom);
-	            if (matched != null){
-	            	AtomTypeManipulator.configure(atom, matched);
-	            }else{
-	            	System.out.println("Could not find matching atom type for atom: " + atom);
-	            }
-	        }
-	        
-			CDKHydrogenAdder hAdder = CDKHydrogenAdder.getInstance(container.getBuilder());
-			hAdder.addImplicitHydrogens(container);
-			AtomContainerManipulator.convertImplicitToExplicitHydrogens(container);
-
-		} catch (Exception e1) {
-			System.out.println("Error addding hydrogens : " + e1.getMessage());
-		}
-
-
-
-	}
-
+                try {
+                        CDKAtomTypeMatcher matcher = CDKAtomTypeMatcher.getInstance(container.getBuilder());
+                for (IAtom atom : container.atoms()) {
+                    IAtomType matched = matcher.findMatchingAtomType(container, atom);
+                    if (matched != null){
+                            AtomTypeManipulator.configure(atom, matched);
+                    }else{
+                            System.out.println("Could not find matching atom type for atom: " + atom);
+                    }
+                }
+                        CDKHydrogenAdder hAdder = CDKHydrogenAdder.getInstance(container.getBuilder());
+                        hAdder.addImplicitHydrogens(container);
+                        AtomContainerManipulator.convertImplicitToExplicitHydrogens(container);
+                } catch (Exception e1) {
+                        System.out.println("Error addding hydrogens : " + e1.getMessage());
+                }
+        }
 }
