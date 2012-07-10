1446778b0b8c0b9c7c50126e8f3161a739ae982b
Ola Spjuth <ola.spjuth@gmail.com>
2012-07-10 09:45:50 +0200

    Sanitize results and replace Infinity with NaN

--- plugins/net.bioclipse.qsar/src/net/bioclipse/qsar/business/QsarManager.java
+++ plugins/net.bioclipse.qsar/src/net/bioclipse/qsar/business/QsarManager.java
@@ -1274,6 +1274,22 @@ public class QsarManager implements IQsarManager{
 			}
 
 		}
+		
+		//Sanitize results, e.g. replace Infinity with NA
+		for (IMolecule mol : allResults.keySet()){
+			List<IDescriptorResult> lres = allResults.get(mol);
+			for (IDescriptorResult dr : lres){
+				Float[] values = dr.getValues();
+				for (int i=0; i< values.length; i++){
+					Float value = values[i];
+					System.out.println(value);
+					if (value.isInfinite()){
+						values[i]=Float.NaN;
+						dr.setValues(values);
+					}
+				}
+			}
+		}
 
 		return allResults;
 
